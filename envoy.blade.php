e99a7fdd3e88f44a79882404efe82d1c86daec6c
Administrator <gitlab_admin_fd4701@example.com>
2026-04-01 12:04:30 +0000

    Add new file

---
+++ envoy.blade.php
@@ -0,0 +1,45 @@
+@servers(['web' => 'gitdep@192.168.56.102'])
+
+@setup
+$repository = 'git clone git@192.168.56.102:nixie/newlara.git';
+$releases_dir = '/var/www/app/releases';
+$app_dir = '/var/www/app';
+$release = date('YmdHis');
+$new_release_dir = $releases_dir.'/'.$release;
+@endsetup
+
+
+@story('deploy')
+    clone_repository
+    run_composer
+    update_symlinks
+@endstory
+
+
+@task('clone_repository')
+    echo 'Cloning repository'
+    [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
+    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+    cd {{ $new_release_dir }}
+    git reset --hard {{ $commit }}
+@endtask
+
+
+@task('run_composer')
+    echo "Starting deployment ({{ $release }})"
+    cd {{ $new_release_dir }}
+    composer install --prefer-dist --no-scripts -q -o
+@endtask
+
+
+@task('update_symlinks')
+    echo 'Linking storage directory'
+    rm -rf {{ $new_release_dir }}/storage
+    ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage
+
+    echo 'Linking .env file'
+    ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env
+
+    echo 'Linking current release'
+    ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
+@endtask
